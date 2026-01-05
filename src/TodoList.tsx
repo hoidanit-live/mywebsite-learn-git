afa21d3c5c13b6c03f8fe7e18908554000a73463
Hỏi Dân IT @hoidanit <186009049+hoidanit-live@users.noreply.github.com>
2026-01-05 14:09:00 +0000

    style: chỉnh spacing footer #20

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -14,7 +14,7 @@ const initialTodos: Todo[] = [
   { id: 3, text: 'Uống nước', done: false, priority: 'normal' },
 ]
 
-// note: sửa lỗi chính tả #16
+// note: chỉnh spacing footer #20
 function TodoList() {
   const [todos, setTodos] = useState<Todo[]>(initialTodos)
   const [input, setInput] = useState('')
