4c801a41d273c8648923f7ceaf0754ad26bce94b
Hỏi Dân IT @hoidanit <186009049+hoidanit-live@users.noreply.github.com>
2026-01-05 04:15:00 +0000

    fix: sửa lỗi chính tả #4

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -14,6 +14,7 @@ const initialTodos: Todo[] = [
   { id: 3, text: 'Uống nước', done: false, priority: 'normal' },
 ]
 
+// note: sửa lỗi chính tả #4
 function TodoList() {
   const [todos, setTodos] = useState<Todo[]>(initialTodos)
   const [input, setInput] = useState('')
