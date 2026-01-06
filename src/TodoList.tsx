f652489bc431048c4f862a3c77bd7250264737c3
Hỏi Dân IT @hoidanit <186009049+hoidanit-live@users.noreply.github.com>
2026-01-06 07:15:00 +0000

    chore: sắp xếp lại import #48

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -14,7 +14,7 @@ const initialTodos: Todo[] = [
   { id: 3, text: 'Uống nước', done: false, priority: 'normal' },
 ]
 
-// note: chỉnh spacing footer #44
+// note: sắp xếp lại import #48
 function TodoList() {
   const [todos, setTodos] = useState<Todo[]>(initialTodos)
   const [input, setInput] = useState('')
