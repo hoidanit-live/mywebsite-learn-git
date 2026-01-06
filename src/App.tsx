4ad5162da1db3393f55c57db0b0c381273aeb037
Hỏi Dân IT @hoidanit <186009049+hoidanit-live@users.noreply.github.com>
2026-01-06 00:57:00 +0000

    feat(app): gắn Cart vào App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Footer from './Footer'
 import TodoList from './TodoList'
+import Cart from './features/cart/Cart'
 
 function App() {
   return (
     <>
       <Header />
       <TodoList />
+      <Cart />
       <Footer />
     </>
   )
